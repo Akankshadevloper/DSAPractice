ba8345101bcff763180754f791f0cf448a6a6d53
Akanksha Kumari <200301120008@cutm.ac.in>
2024-06-12 11:37:25 +0530

    file added

--- src/ArrayAndArrayList/Max.java
+++ src/ArrayAndArrayList/Max.java
@@ -2,6 +2,19 @@ package ArrayAndArrayList;
 
 public class Max {
     public static void main(String[] args) {
-        
+        int[] arr = { 1 , 3 , 23 , 9 , 10};
+        System.out.println(max(arr));
     }
+
+
+    static int max(int[] arr) {
+        int max = arr[0];
+       for (int i = 1; i < arr.length; i++) {
+           if(max < arr[i]) {
+               max = arr[i];
+           }
+       }
+       return max;
+    }
+
 }
