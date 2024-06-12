90b3a3e0348e0a55f2581e552373640715d5213b
Akanksha Kumari <200301120008@cutm.ac.in>
2024-06-12 11:55:56 +0530

    file added

--- src/ArrayAndArrayList/MaxRange.java
+++ src/ArrayAndArrayList/MaxRange.java
@@ -6,6 +6,13 @@ public class MaxRange {
         System.out.println(maxRange(arr, 2 ,5));
     }
     static int maxRange(int[] arr, int start, int end) {
+
+        if(end > start){
+            return -1;
+        }
+        if(arr== null){
+            return 0;
+        }
         int max = arr[start];
         for (int i = start; i < end; i++) {
            if (arr[i] > max) {
