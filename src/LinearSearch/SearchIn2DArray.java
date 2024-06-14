f1e2fe8c81f9199f9e8a712eb11bb013bba1aa5a
Akanksha Kumari <200301120008@cutm.ac.in>
2024-06-14 13:33:45 +0530

    file added

--- src/LinearSearch/SearchIn2DArray.java
+++ src/LinearSearch/SearchIn2DArray.java
@@ -13,6 +13,10 @@ public class SearchIn2DArray {
         int target = 34;
         int[] ans = search(arr,target);
         System.out.println(Arrays.toString(ans));
+
+        System.out.println(max(arr));
+
+        System.out.println(Integer.MIN_VALUE); // Minimum value of java can hold is this
     }
     static int[] search(int[][] arr, int target) {
         for(int row =0; row < arr.length; row++) {
@@ -25,5 +29,22 @@ public class SearchIn2DArray {
         return new int[]{-1,-1};
     }
 
-
+    static int max(int[][] arr) {
+         int max = Integer.MIN_VALUE;
+        for (int[] element : arr) {
+            for (int i : element) {
+                if (i > max) {
+                    max = i;
+                }
+            }
+            //or
+//            for(int row =0; row < arr.length; row++) {
+//                for (int col = 0; col < arr[row].length; col++) {
+//                    if(arr[row][col] > max) {
+//                        max = arr[row][col];
+//                    }
+//                }
+        }
+        return max;
+    }
 }
