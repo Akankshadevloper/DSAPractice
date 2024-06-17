5c78dd3f3a6c9c8b03c78b8972424a99d709a0c3
Akanksha Kumari <200301120008@cutm.ac.in>
2024-06-17 13:19:19 +0530

    file added

---
+++ src/BinarySearch/FirstAndLast34.java
@@ -0,0 +1,46 @@
+package BinarySearch;
+
+public class FirstAndLast34 {
+    public static void main(String[] args) {
+       int[] arr   = {5,7,7,8,8,10};
+       int target = 8;
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] ans = {-1, -1};
+        // check for first occurrence if target first
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+    // this function just returns the index value of target
+    int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
--- src/BinarySearch/SmallestNumGreater.java
+++ src/BinarySearch/SmallestNumGreater.java
@@ -3,7 +3,6 @@ package BinarySearch;
 public class SmallestNumGreater {
     public static void main(String[] args) {
         int [] arr = {'c','f','j'};
-
     }
 
     public char nextGreatestLetter(char[] letters, char target) {
