2e041a04bf90c81e0e0a7221e2130309cb566084
Akanksha Kumari <200301120008@cutm.ac.in>
2024-07-26 19:41:11 +0530

    file added

---
+++ src/BinarySearch/findMedianSortedArrays.java
@@ -0,0 +1,36 @@
+package BinarySearch;
+
+public class findMedianSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0, high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int maxY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+
+            int minX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+            int minY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxX <= minY && maxY <= minX) {
+                if ((x + y) % 2 == 0) {
+                    return ((double)Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
+                } else {
+                    return (double)Math.max(maxX, maxY);
+                }
+            } else if (maxX > minY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+}
