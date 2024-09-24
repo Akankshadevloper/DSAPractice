15b246dfbe11ecbaba3115e71d65dfaacd88f76c
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-24 09:14:54 +0530

    conditional assignment

---
+++ src/Conditional  Assignment/AdditionOfTwoNumber.java
@@ -0,0 +1,15 @@
+package AssigmentConditionalStatement;
+
+import java.util.Scanner;
+
+public class AdditionOfTwoNumber {
+    public static void main(String[] args) {
+        //Addition Of Two Numbers
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
+
