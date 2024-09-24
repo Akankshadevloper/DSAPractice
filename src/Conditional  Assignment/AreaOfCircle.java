ded020de9c551073c1997c228f5d51bbf07ec4a5
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-24 09:15:39 +0530

    area of a circle

---
+++ src/Conditional  Assignment/AreaOfCircle.java
@@ -0,0 +1,14 @@
+package AssigmentConditionalStatement;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int r = sc.nextInt();
+      float pi = 3.14F;
+      double area =  (3.14 * r * r);
+      System.out.println("Area of circle   = " + area );
+  }
+
+}
