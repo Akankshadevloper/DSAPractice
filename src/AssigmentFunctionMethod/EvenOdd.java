abe9c2db27423dcb27dd464a035698cdaf09a11d
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-25 23:04:22 +0530

    evenOdd

---
+++ src/AssigmentFunctionMethod/EvenOdd.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+//Write a program to print whether a number is even or odd, also take input from the user.
+
+    }
+}
