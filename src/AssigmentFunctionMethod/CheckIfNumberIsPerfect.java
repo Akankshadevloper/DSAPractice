66df92a37b0bbaccd16987dc6d9c3902d82e179b
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-25 23:02:51 +0530

    numberIsPerfectOrNot

---
+++ src/AssigmentFunctionMethod/CheckIfNumberIsPerfect.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class CheckIfNumberIsPerfect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
