e317d250858c7d344d94124683223ca24afbd545
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-26 22:57:48 +0530

    simple Interest

---
+++ src/AssigmentFunctionMethod/SimpleInterest.java
@@ -0,0 +1,8 @@
+package Assignment;
+
+public class SimpleInterest {
+    //Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+    public static void main(String[] args) {
+
+    }
+}
