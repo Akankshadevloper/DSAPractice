0fbe06896061f1ee86176832de3039c450e28f49
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-26 22:59:59 +0530

    palindrom

---
+++ src/AssigmentFunctionMethod/Palindrome.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+
+//A palindrome is a word, phrase, number, or any sequence of characters that reads the same forward and backward, ignoring spaces, punctuation,
+// and capitalization.
+//---> example:- level , 121 , radar , madam , 12321
+
+public class Palindrome {
+    //To find out whether the given String is Assignment.Palindrome or not.
+    public static void main(String[] args) {
+
+    }
+}
