f03bb131eb9a7186f9e0c9bdcd057d113b2e15f5
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-27 18:49:31 +0530

    fibonumber

---
+++ src/AssigmentFunctionMethod/FibonacciSeries.java
@@ -0,0 +1,10 @@
+package Assignment;
+
+
+// 0 1 1 2 3 5 8 13 21...............
+public class FibonacciSeries {
+   //To calculate Fibonacci Series up to n numbers.
+    public static void main(String[] args) {
+
+    }
+}
