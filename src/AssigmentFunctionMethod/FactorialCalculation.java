b6d0ea4c43348c09587a8f33771eeb7bf320329b
Akanksha Kumari <200301120008@cutm.ac.in>
2024-09-27 18:50:33 +0530

    factorial number

---
+++ src/AssigmentFunctionMethod/FactorialCalculation.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+
+import java.util.Scanner;
+
+// 5! = 5*4*3*2*1
+public class FactorialCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+
+
+    }
+}
