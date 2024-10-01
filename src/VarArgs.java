076a256b6ab33a933b3e3ec344660d1b564262a0
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-01 20:19:05 +0530

    VarArgument

---
+++ src/VarArgs.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+//        fun(2,3,4,5,6,7,8,9,10);
+        multiple(2,4, "akanksha", "bibek","sattu");
+    }
+
+    static void multiple(int a , int b , String ...v){
+
+    }
+
+//    static void fun(int ...v){
+//        System.out.println(Arrays.toString(v));
+//    }
+}
