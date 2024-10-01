a547157d049ca8f3214747d5446b6b7c379f6ded
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-01 20:16:30 +0530

    Basic While

---
+++ src/BasicWhile.java
@@ -0,0 +1,9 @@
+public class BasicWhile {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count != 5) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
