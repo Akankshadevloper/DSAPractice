15709112727691f64b45686c6cf5a7366aeb3f06
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-02 21:26:27 +0530

    InputFilee

---
+++ src/InputFile.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class InputFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rollNumber = sc.nextInt();
+        System.out.println("Your roll number is " + rollNumber);
+    }
+}
