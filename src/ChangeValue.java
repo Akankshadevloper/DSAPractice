86dad5bc0c9df9cba407a29f2b1737b7e208dbdc
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-03 21:31:16 +0530

    changevalue

---
+++ src/ChangeValue.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ChangeValue {
+    public static void main(String[] args) {
+
+        //create an array
+        int[] arr = {1, 3, 2, 45, 6};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change(int[] num) {
+        num[0] = 99;
+    }
+}
