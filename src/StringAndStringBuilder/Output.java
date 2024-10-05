a5797d30e4be51e3d2904ba66a280aa5a7fae51a
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-05 22:27:23 +0530

    Output

---
+++ src/StringAndStringBuilder/Output.java
@@ -0,0 +1,19 @@
+package StringAndStringBuilder;
+
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+
+
+
+
+//        System.out.println("Akanksha");
+
+        System.out.println(Arrays.toString(new int[]{2,3,4,5,6,7,8,9}));
+
+        String name = null;
+        System.out.println(name);
+    }
+}
