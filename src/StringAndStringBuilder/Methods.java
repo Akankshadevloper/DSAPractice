b23716a070d03cc270683e8c5ebc515e3b2b14b7
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-05 22:28:43 +0530

    Methods

---
+++ src/StringAndStringBuilder/Methods.java
@@ -0,0 +1,18 @@
+package StringAndStringBuilder;
+
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Akanksha Kumari";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toUpperCase());
+        System.out.println(name);
+        System.out.println(name.indexOf('a'));
+        System.out.println("        ak      ".strip());
+        System.out.println(Arrays.toString(name.split(" " )));
+    }
+}
+
+//assignment
+
