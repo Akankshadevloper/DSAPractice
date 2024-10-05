7a53ba979de2bff3281fbad6898fbde73307731b
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-05 22:25:05 +0530

    prettyPrinting

---
+++ src/StringAndStringBuilder/PrettyPrinting.java
@@ -0,0 +1,15 @@
+package StringAndStringBuilder;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.145f;
+//        System.out.printf("Formatted number is %.2f" , a);
+
+
+//        System.out.println(Math.PI);
+
+//        System.out.printf("Pie: %.3f", Math.PI); //round off
+
+        System.out.printf("Hello my name is %s and I am %s", "Akanksha", "Cool");
+    }
+}
