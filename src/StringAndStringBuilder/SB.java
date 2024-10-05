da8faa8218275e0da2ff4e88d519d84132c956fb
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-05 22:22:56 +0530

    SingBuilder

---
+++ src/StringAndStringBuilder/SB.java
@@ -0,0 +1,15 @@
+package StringAndStringBuilder;
+
+public class SB {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            builder.append(ch);
+        }
+        System.out.println(builder);
+    }
+}
+//this is not creating a new object
+//string builder is mutable
+//this is changing the same object and adding all the alphabet into it..
