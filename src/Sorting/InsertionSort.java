b0520adfe3f8eb914dca16c08b21dfcb02c78ae0
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-06 22:43:07 +0530

    InsertionSort

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,5,1,2,3};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(arr[j] < arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+}
