c51a730417a823ac16be159e6442dc7973ec8e44
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-11 22:09:06 +0530

    Pattern31

---
+++ src/Pattern/Pattern31.java
@@ -0,0 +1,14 @@
+package Pattern;
+
+public class Pattern31 {
+    public static void main(String[] args){
+
+    }
+    static void pattern31(int n ){
+        for(int row = 0; row < 2*n-1; row++ ){
+            for(int col = 0; col < 2 * n - 1 ; col++){
+
+            }
+        }
+    }
+}
