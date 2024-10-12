be7581cd4fe794232d50addcfeb55042951c3cff
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-12 20:22:27 +0530

    DoWhileLoop

---
+++ src/Loop/DoWhileLoop.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class DoWhileLoop {
+     public static void main(String[] args) {
+         int n = 1;
+         do{
+             System.out.println(n);
+             n++;
+         }while(n <= 5);
+     }
+}
