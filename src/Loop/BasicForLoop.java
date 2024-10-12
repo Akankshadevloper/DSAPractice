cc4397785e13fab45dc4f8ef278451ddb34a31bc
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-12 20:21:24 +0530

    ForLoop

---
+++ src/Loop/BasicForLoop.java
@@ -0,0 +1,9 @@
+package Loop;
+
+public class BasicForLoop {
+    public static void main(String[] args) {
+        for(int count =1 ; count !=5; count++){
+            System.out.println(count);
+        }
+    }
+}
