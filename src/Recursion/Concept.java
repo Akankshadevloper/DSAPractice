b52887ee6c2e9016f7b70126aeb1c35fb4bf2ae8
Akanksha Kumari <200301120008@cutm.ac.in>
2024-10-13 21:36:31 +0530

    Concept

---
+++ src/Recursion/Concept.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Concept {
+    public static void main(String[] args) {
+        fun(5);
+    }
+    static void fun(int n) {
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+//        fun(n--);
+        fun(--n);
+    }
+}
