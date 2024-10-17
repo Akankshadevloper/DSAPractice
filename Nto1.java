f98d276da23bcf78aa2bb7b98efac4b121ab32a4
Akanksha Kumari <110593751+Akankshadevloper@users.noreply.github.com>
2024-10-17 09:38:02 +0530

    Add files via upload

---
+++ Nto1.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        fun(5);
+    }
+
+    static void fun(int n ){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+}
